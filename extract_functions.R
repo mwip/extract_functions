6fac4860adb65bfa0f0c330fc05067c1cf4d6a92
Matthias Weigand <matthias.weigand@dlr.de>
2020-06-29 15:26:21 +0200

    Hide helper functions by prepending "."

--- extract_functions.R
+++ extract_functions.R
@@ -15,7 +15,7 @@ extract_functions <- function(x, evaluate = TRUE, verbose = TRUE,
       # if not, check if current_line is the beginning of a function
       if (str_detect(current_line, "<-[[:blank:]]*function[[:blank:]]*\\(")) {
         # and add the current_line to the next list item of extracted_functions
-        extracted_functions <- append_to_extracts(extracted_functions, 
+        extracted_functions <- .append_to_extracts(extracted_functions, 
                                                   current_line, where = "new")
         if (str_detect(current_line, "\\{")) {
           # no longer in definition
@@ -25,14 +25,14 @@ extract_functions <- function(x, evaluate = TRUE, verbose = TRUE,
           # if so indicate
           in_function <- TRUE
           # increase the brace_count
-          brace_count <- brace_count + count_braces(current_line)
+          brace_count <- brace_count + .count_braces(current_line)
         } else {
           in_definition <- TRUE
         }
       } else {
         if (in_definition) {
         # and add the current_line to the last list item of extracted_functions
-        extracted_functions <- append_to_extracts(extracted_functions, 
+        extracted_functions <- .append_to_extracts(extracted_functions, 
                                                   current_line, where = "last")
           if (str_detect(current_line, "\\{")) {
             # no longer in definition
@@ -42,29 +42,29 @@ extract_functions <- function(x, evaluate = TRUE, verbose = TRUE,
             # if so indicate
             in_function <- TRUE
             # increase the brace_count
-            brace_count <- brace_count + count_braces(current_line)
+            brace_count <- brace_count + .count_braces(current_line)
           }
         }
       }
     } else {
       # if already in a function, first update brace count with the current_line
-      brace_count <- brace_count + count_braces(current_line)
+      brace_count <- brace_count + .count_braces(current_line)
       
       # if brace_count is higher than 0, function has not ended yet
       if (brace_count > 0) {
         # append line to current item of extracted_functions
-        extracted_functions <- append_to_extracts(extracted_functions, 
+        extracted_functions <- .append_to_extracts(extracted_functions, 
                                                   current_line, where = "last")
         
       # if brace_count is 0 the function has ended correctly
       } else if (brace_count == 0) {
         in_function <- FALSE
-        extracted_functions <- append_to_extracts(extracted_functions, 
+        extracted_functions <- .append_to_extracts(extracted_functions, 
                                                   current_line, where = "last")
         
       # else the function has ended incorrectly
       } else {
-        extracted_functions <- append_to_extracts(extracted_functions, 
+        extracted_functions <- .append_to_extracts(extracted_functions, 
                                                   where = "corrupt")
         brace_count <- 0
         in_function <- FALSE
@@ -98,7 +98,7 @@ extract_functions <- function(x, evaluate = TRUE, verbose = TRUE,
 }
 
 
-count_braces <- function(x) {
+.count_braces <- function(x) {
   opening_braces <- sum(nchar(unlist(str_extract_all(x, "\\{"))))
   opening_braces <- ifelse(length(opening_braces) == 1, opening_braces, 0)
   closing_braces <- sum(nchar(unlist(str_extract_all(x, "\\}"))))
@@ -108,7 +108,7 @@ count_braces <- function(x) {
   return(out)
 }
 
-append_to_extracts <- function(extracts_list, x, where) {
+.append_to_extracts <- function(extracts_list, x, where) {
   if (where == "last") {
     extracts_list[[length(extracts_list)]] <- 
       c(extracts_list[[length(extracts_list)]], x)
